d6dc0522e66d78326909d8a17f76d6e79c7486e9
Graham Gibney <graham.gibney@mycit.ie>
2021-04-15 23:59:24 +0100

    Testing the choices and functions to make sure they perform properly

--- RunnerProjectCode/Graham_Gibney_Runnerz.py
+++ RunnerProjectCode/Graham_Gibney_Runnerz.py
@@ -17,9 +17,28 @@ def mainMenuChoice():
     main_choice = runnerFunctions.get_choice_1_7("Please enter your choice: ")
     return main_choice
 
+
+def performMainChoice(main_menu_choice):
+    if main_menu_choice == 1:
+        print("1")
+    elif main_menu_choice == 2:
+        print("2")
+    elif main_menu_choice == 3:
+        print("3")
+    elif main_menu_choice == 4:
+        print("4")
+    elif main_menu_choice == 5:
+        print("5")
+    elif main_menu_choice == 6:
+        print("6")
+    else:
+        print("Thanks")
+
+
 def main():
     displayMenu()
     main_menu_choice = mainMenuChoice()
+    performMainChoice(main_menu_choice)
 
 
 if __name__ == '__main__':
