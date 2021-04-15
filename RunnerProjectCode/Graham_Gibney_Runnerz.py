3eb19f65fbd205abbfe287923206cf3f04f6769a
Graham Gibney <graham.gibney@mycit.ie>
2021-04-15 22:57:25 +0100

    Starting off with Printing the Main Menu

---
+++ RunnerProjectCode/Graham_Gibney_Runnerz.py
@@ -0,0 +1,20 @@
+import runnerFunctions
+
+def displayMenu():
+    print()
+    print("Main Menu")
+    print("=" * 9)
+    print("1. Show the results for a race")
+    print("2. Add the results for a race")
+    print("3. Show all competitors by country")
+    print("4. Show the winner of each race")
+    print("5. Show all the race times for one competitor")
+    print("6. Show all competitors who have won a race")
+    print("7. Quit")
+
+def main():
+    displayMenu()
+
+
+if __name__ == '__main__':
+    main()
